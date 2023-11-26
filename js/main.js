23e6ab2642ebcc8c6a42c049f484babbbc9a88e3
Trita Valentin <tritavalentin@icloud.com>
2023-11-26 21:02:52 +0200

    status3

--- js/main.js
+++ js/main.js
@@ -5,3 +5,35 @@ function checkUserLogin() {
         window.location.href = './login.html';
     }
 }
+
+const employeesArr = [
+    {
+       name: 'Alex',
+       age: '28',
+       project: null, 
+       birthdate: '1995-01-10',
+       date_emp: '2020-10-05',
+       phone: '0743282329',
+       email: 'alex@icloud.com'
+    },
+    {
+        name: 'radu',
+        age: '25',
+        project: null, 
+        birthdate: '1998-01-10',
+        date_emp: '2020-11-05',
+        phone: '0747282329',
+        email: 'radu@icloud.com'
+     },
+     {
+        name: 'Mada',
+        age: '24',
+        project: null, 
+        birthdate: '1999-01-11',
+        date_emp: '2023-10-05',
+        phone: '0743265329',
+        email: 'Mada@icloud.com'
+     },
+    
+
+];
