adf1336a996e72e3d462a45e6f3f6768216411db
Trita Valentin <tritavalentin@icloud.com>
2023-11-30 19:06:04 +0200

    status30.11

--- js/main.js
+++ js/main.js
@@ -71,3 +71,75 @@ function createRow(person, i) {
     rowStr += '</tr>';
     return rowStr;
 }
+function displayAddForm() {
+    document.getElementById('add_form_container').style.display = 'block';
+    document.getElementById('add_container').style.display = 'none';
+}
+    function cancelAddForm() {
+        const userConfirm = confirm('Are you sure you want to cancel? ')
+
+        if (userConfirm) {
+            document.getElementById('add_form').reset();
+            document.getElementById('add_form_container').style.display = 'none';
+            document.getElementById('add_container').style.display = 'block';
+        }
+    }
+
+    function addNewUser() {
+        console.log('adding...');
+    }
+
+    function checkName() {
+        const name_el = document.getElementById('name');
+        const name = name_el.value;
+        console.log('value', name);
+        if(name === '' || name === null) {
+            document.getElementById('name_err').style.display = 'block';
+            name_el.classList.add('input_err');
+        } else {
+            document.getElementById('name_err').style.display = 'none';
+            name_el.classList.remove('input_err');
+        
+       }
+    }
+
+    function checkAge() {
+        const age_el = document.getElementById('age');
+        const age = age_el.value;
+
+       
+        if(age !== '' && !isNaN(age) && age >= 18 && age <= 65) {
+            document.getElementById('age_err').style.display = 'none';
+            age_el.classList.remove('input_err');        
+        } else {
+            document.getElementById('age_err').style.display = 'block';
+            age_el.classList.remove('input_err'); 
+        
+       }
+    }
+        function checkElement(element) {
+         const html_el = document.getElementById(element);
+         const el_value = html_el.value;
+
+         let patt;
+
+         if (element === 'birthdate') {
+            patt = /^\d{4}-\d{2}-\d{2}$/g;
+
+         } else if (element === 'phone') {
+            patt = /^07\d{8}/g;
+         } else if (element === 'email') {
+            patt = /^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/g;
+         }
+
+         //^\d{4}-\d{2}-\d{2}$/g;
+
+         if(el_value === '' ||  !patt.test(el_value)) {
+            document.getElementById(element + '_err').style.display = 'block';
+            html_el.classList.add('input_err');
+        } else {
+            document.getElementById(element + '_err').style.display = 'none';
+            html_el.classList.remove('input_err');
+        
+       }
+    }
